c2816e93b871cf197f534bf2983afc59787e6181
Xavier Legault <legault.xavier@live.ca>
2025-04-10 20:36:48 -0400

    Add finising touche

--- Coquille-Prolog.pl
+++ Coquille-Prolog.pl
@@ -45,12 +45,11 @@ assert(fait(r(a))).
 
 si saison(hiver) alors neige().
 si not(saison(hiver)) ou saison(printemps) ou saison(ete) ou saison(automne) alors not(neige()).
-
 si neige() et type(sport) et prix(payant) alors activite(ski).
 si neige() et prix(gratuit) et lieu(quebec) alors activite(carnaval).
 si saison(ete) et prix(payant) alors activite(parc_aquatique).
 si saison(automne) et public(enfant) alors activite(halloween).
-si type(recreatif) et prix(payant) alors activite(musee).
+si type(recreatif) et public(adulte) et prix(payant) alors activite(musee).
 si type(recreatif) et lieu(quebec) ou prix(gratuit) alors activite(marche_vieux_quebec).
 si type(recreatif) et prix(payant) alors activite(aquarium).
 si not(neige()) et lieu(hors_quebec) alors activite(camping).
@@ -58,41 +57,85 @@ si not(neige()) et type(sport) alors activite(velo).
 si public(enfant) alors activite(recreofun).
 
 /*
-assert(fait(saison(ete))).
-
+Test 1
+========================================
 assert(fait(saison(hiver))).
 assert(fait(type(sport))).
 assert(fait(prix(payant))).
+
+
+Test 2
+========================================
+assert(fait(lieu(quebec))).
+assert(fait(public(enfant))).
+assert(fait(type(recreatif))).
+
+
+Test 3
+========================================
+assert(fait(saison(automne))).
+assert(fait(lieu(hors_quebec))).
+
+
+Test 4
+========================================
+assert(fait(saison(ete))).
+assert(fait(type(sport))).
+
+
+Test 5
+========================================
+assert(fait(type(recreatif))).
+assert(fait(prix(payant))).
+assert(fait(public(adulte))).
 */
 
 def_activite(ski, hiver, "Mnt. Ste-Anne").
 def_activite(carnaval, hiver, quebec).
 def_activite(parc_aquatique, ete, quebec).
 def_activite(halloween, automne, quebec).
+
 def_activite(musee, hiver, quebec).
 def_activite(musee, printemps, quebec).
 def_activite(musee, ete, quebec).
 def_activite(musee, automne, quebec).
+
 def_activite(marche_vieux_quebec, hiver, quebec).
 def_activite(marche_vieux_quebec, printemps, quebec).
 def_activite(marche_vieux_quebec, ete, quebec).
 def_activite(marche_vieux_quebec, automne, quebec).
+
 def_activite(aquarium, hiver, quebec).
 def_activite(aquarium, printemps, quebec).
 def_activite(aquarium, ete, quebec).
 def_activite(aquarium, automne, quebec).
+
+def_activite(velo, printemps, quebec).
+def_activite(velo, ete, quebec).
+def_activite(velo, automne, quebec).
+
 def_activite(camping, printemps, hors_quebec).
 def_activite(camping, ete, hors_quebec).
 def_activite(camping, automne, hors_quebec).
 
+def_activite(recreofun, hiver, quebec).
+def_activite(recreofun, printemps, quebec).
+def_activite(recreofun, ete, quebec).
+def_activite(recreofun, automne, quebec).
+
 plan() :-
 	ch_avant,
-	fait(activite(N)),
-	def_activite(N, S, L),
+	findall(A, fait(activite(A)), L),
+	write_result(L).
+
+write_result([]).
+write_result([N | L]) :-
+	findall(Se, def_activite(N, Se, _), S),
+	findall(Lie, def_activite(N, _, Lie), [Li | _]),
 	write("Activité : "),
-	write(N), nl(),
+	write(N), write(", "),
 	write("Saison : "),
-	write(S), nl(),
+	write(S), write(", "),
 	write("Lieu : "),
-	write(L), nl()
-. 
+	write(Li), nl(),
+	write_result(L).
