c683b05ecabf656eab8f60d9d6d691773bff9d11
Xavier Legault <legault.xavier@live.ca>
2025-04-24 13:41:22 -0400

    More sexy code

--- Equipe_12.pl
+++ Equipe_12.pl
@@ -6,17 +6,19 @@ p(R) -->  groupe_adjectif(A), groupe_nominal(N), { R = [A, N] }.
 
 groupe_nominal(R) --> nom(R).
 groupe_nominal(R) --> determinant(), adjectif(A), nom(N), { R = [A, N] }.
+groupe_nominal(R) --> nom(R), adjectif(_).
 groupe_nominal(R) --> preposition(), determinant(), nom(R).
 groupe_verbe() --> pronom(), verbe().
 groupe_adjectif(R) --> adverbe(), adjectif(R).
 groupe_adjectif(R) --> adjectif(R).
-nom(sujet('carte graphique')) --> ['carte graphique'].
+nom(sujet(carte)) --> [carte].
 nom(sujet(achat)) --> [achat].
 pronom() --> [ceci].
 verbe() --> [est].
 adjectif(niveau(superbe)) --> [superbe].
 adjectif(niveau(satisfait)) --> [satisfait].
 adjectif(niveau(bon)) --> [bon].
+adjectif(niveau()) --> [graphique].
 adverbe() --> [très].
 determinant() --> [mon].
 determinant() --> [un].
