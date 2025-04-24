c87d28d09fca10d26d7361f8ec067e75aacdd947
Xavier Legault <legault.xavier@live.ca>
2025-04-24 13:45:35 -0400

    Commented and all

--- Equipe_12.pl
+++ Equipe_12.pl
@@ -1,25 +1,58 @@
 
 
 
+##################################################
+#					Equipe 12					 #
+##################################################
+
+
+
+
+
+
+##################################################
+# Predicat pour savoir le niveau de satisfaction #
+# d'un sujet selon la phrase.                    #
+##################################################
 p(R) -->  groupe_verbe(), groupe_nominal(N), { R = [N] }.
 p(R) -->  groupe_adjectif(A), groupe_nominal(N), { R = [A, N] }.
 
+##################################################
+# Groupe nomninale de la phrase                  #
+##################################################
 groupe_nominal(R) --> nom(R).
 groupe_nominal(R) --> determinant(), adjectif(A), nom(N), { R = [A, N] }.
 groupe_nominal(R) --> nom(R), adjectif(_).
 groupe_nominal(R) --> preposition(), determinant(), nom(R).
+
+##################################################
+# Groupe vervable de la phrase                   #
+##################################################
 groupe_verbe() --> pronom(), verbe().
+
+##################################################
+# Groupe d'adjective de la phrase                #
+##################################################
 groupe_adjectif(R) --> adverbe(), adjectif(R).
 groupe_adjectif(R) --> adjectif(R).
+
+##################################################
+# Base de connaissance grammaticale              #
+##################################################
 nom(sujet(carte)) --> [carte].
 nom(sujet(achat)) --> [achat].
+
 pronom() --> [ceci].
+
 verbe() --> [est].
+
 adjectif(niveau(superbe)) --> [superbe].
 adjectif(niveau(satisfait)) --> [satisfait].
 adjectif(niveau(bon)) --> [bon].
 adjectif(niveau()) --> [graphique].
+
 adverbe() --> [très].
+
 determinant() --> [mon].
 determinant() --> [un].
 preposition() --> [de].
